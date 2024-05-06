8d3479bf8eb16c531be04250d7752d66ff1e1ae8
tfugelsang <tomfugelsang@protonmail.com>
2024-05-05 22:33:26 -0600

    v1

---
+++ import array as myarray.py
@@ -0,0 +1,14 @@
+import array as myarray
+#Creting an array
+arr = myarray.arry('i' , [1,2,3 ])
+arr1 = myarray.array('d', [1.0, 2.1, 2.4, 2.5, 2.6])
+arr2 = myarray.array('u', ['a' 'b','c', 'd'])
+#typecodes
+print(arr.typecode)
+print(arr1.typecode)
+print(arr2.typecode)
+#first look at the array
+print(arr)
+print(arr1)
+print(arr2)
+
